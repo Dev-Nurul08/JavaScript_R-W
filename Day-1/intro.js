2d992599babe0b6bace96a9d22a0cdcc6db1a265
Nurulshaikh786 <shaikhnurul8200@gmail.com>
2025-08-22 23:22:38 +0530

    java-code

---
+++ Day-1/intro.js
@@ -0,0 +1,19 @@
+// Swapping with javascript
+
+console.log("Hello, World!");
+
+let a=20;
+let b=20;
+
+
+console.log(a);
+console.log(b);
+
+a=a+b;
+b=a-b;
+a=a-b;
+
+console.log(a);
+console.log(b);
+
+
