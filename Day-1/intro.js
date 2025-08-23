8cae5949ec6b767f38bcc642ffdec783f2fb2341
Nurulshaikh786 <shaikhnurul8200@gmail.com>
2025-08-23 20:59:15 +0530

    push-me

--- Day-1/intro.js
+++ Day-1/intro.js
@@ -1,19 +1,4 @@
-// Swapping with javascript
-
-console.log("Hello, World!");
-
-let a=20;
-let b=20;
-
-
-console.log(a);
-console.log(b);
-
-a=a+b;
-b=a-b;
-a=a-b;
-
-console.log(a);
-console.log(b);
-
+console.log("Hero World");
+name="shaikh nurul";
+console.log(name);
 
