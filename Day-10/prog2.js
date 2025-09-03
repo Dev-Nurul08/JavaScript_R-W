f26a26549694bad95c3fece36fcd910ff0209795
Dev-Mjarir08 <shaikhnurul8200@gmail.com>
2025-09-03 12:56:54 +0530

    push

--- Day-10/prog2.js
+++ Day-10/prog2.js
@@ -1,15 +1,1 @@
-let a=0 , b=1 ,c;
-let str = "";
 
-for(let i = 1 ; i <= 10
-     ; i++){
-         
-    str += `${a} `;
-    c = a + b;
-    a = b;
-    b = c;
-
-    
-    
-}
-console.log(str);
